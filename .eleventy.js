a3714e8b3c37e9fe5214165e165d6204dc4b2771
Yavor Kolev <yavko@outlook.com>
2021-04-14 13:39:57 -0700

    Config

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("static");
+
+    // You can return your Config object (optional).
+    return {
+      dir: {
+        input: "src",
+        output: "dist"
+      }
+    };
+  };
